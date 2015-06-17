8e577cc02f4632fa161516b56eb8493730010ea8
Chris <chris@azureblue.org>
2015-06-17 21:31:20 +0100

    Dictionary literal, migrating to Makefile

--- imageResizer/main.m
+++ imageResizer/main.m
@@ -7,13 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
-@import AppKit;
+#import <AppKit/Appkit.h>
 
 #import "NSImage+MGCropExtensions.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
+
+        // TODO : Proper command line arguments for:
+        //        - targetSize
+        //        - JPEG quality
+
         CGFloat targetSize = 640.0f;
 
         if (argc == 2)
@@ -27,11 +31,11 @@ int main(int argc, const char * argv[]) {
 
         // Make an image out of it
         NSImage *original = [[NSImage alloc] initWithData:inputData];
-        
+
         // Work out target dimensions
         NSSize size = [original size];
-        
-        
+
+
         if (size.width > size.height)
         {
             size.height = targetSize * size.height / size.width;
@@ -40,23 +44,28 @@ int main(int argc, const char * argv[]) {
             size.width = targetSize * size.width / size.height;
             size.height = targetSize;
         }
-        
+
         // Get a scaled version of our image
         NSImage *scaled = [original imageScaledToFitSize:size];
-        
-        // Get a JPEG representation of that image
+
+        // Getting a JPEG representation of that image is a multi-step process...
+
+        // ...TIFF first
         NSData *imageData = [scaled TIFFRepresentation];
+
+        // ...then bitmap
         NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithData:imageData];
+
+        // ... and finally JPEG
         NSNumber *compressionFactor = [NSNumber numberWithFloat:0.9];
-        NSDictionary *imageProps = [NSDictionary dictionaryWithObject:compressionFactor
-                                                               forKey:NSImageCompressionFactor];
-        
+        NSDictionary *imageProps = @{NSImageCompressionFactor: compressionFactor};
+
         imageData = [imageRep representationUsingType:NSJPEGFileType properties:imageProps];
-        
+
         // Write the JPEG data to sdout
         NSFileHandle *output = [NSFileHandle fileHandleWithStandardOutput];
         [output writeData:imageData];
-        
+
     }
     return 0;
 }
